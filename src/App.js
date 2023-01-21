5157b20fcddcfd6d9f9279f4948f5bf42d22ce34
mariosknl <mariosknl@outlook.com>
2023-01-21 12:23:18 +0200

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+import Product from "./components/Product";
 
 // ABIs
-import Dappazon from './abis/Dappazon.json'
+import Dappazon from "./abis/Dappazon.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+	const [account, setAccount] = useState(null);
 
-  return (
-    <div>
+	const loadBlockchainData = async () => {};
 
-      <h2>Welcome to Dappazon</h2>
+	useEffect(() => {
+		loadBlockchainData();
+	}, []);
 
-    </div>
-  );
+	return (
+		<div>
+			<Navigation account={account} setAccount={setAccount} />
+			<h2>Welcome to Dappazon</h2>
+		</div>
+	);
 }
 
 export default App;
