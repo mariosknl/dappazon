ed51ee0cf9f282a4cd3a45c3cdf11bd489f1fe1a
mariosknl <mariosknl@outlook.com>
2023-01-21 10:29:08 +0200

    create contract

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,27 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+	return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
+	let dappazon;
+	let deployer;
+	let buyer;
 
-})
+	beforeEach(async () => {
+		// Setup Accounts
+		[deployer, buyer] = await ethers.getSigners();
+
+		// Deployments
+		const Dappazon = await ethers.getContractFactory("Dappazon");
+		dappazon = await Dappazon.deploy();
+	});
+
+	describe("Deployment", () => {
+		it("sets the owner", async () => {
+			expect(await dappazon.owner()).to.equal(deployer.address);
+		});
+	});
+});
